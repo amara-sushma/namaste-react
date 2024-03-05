4074c088d22ea5b38aec4775c14222cf63c58a86
amara-sushma <vsushma@g.clemson.edu>
2024-03-05 16:09:37 -0600

    initial commit

---
+++ App.js
@@ -0,0 +1,30 @@
+
+const parent = React.createElement(
+    "div",
+    {id: "parent"}, [
+        React.createElement(
+            "div" , 
+            {id: "child"}, 
+            [ React.createElement("h1", {} , "Im h1 tag") , React.createElement("h2", {} , "Im h2 tag")] 
+        ),
+        React.createElement(
+            "div" , 
+            {id: "child"}, 
+            [ React.createElement("h1", {} , "Im h1 tag") , React.createElement("h2", {} , "Im h2 tag")] 
+        )]
+);
+
+
+
+
+const heading = React.createElement(
+    "h1", 
+    {id : "heading", xyz : "abc"}, 
+    "Hello World from React!"
+);
+
+console.log(parent)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
